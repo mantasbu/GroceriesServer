273c83bbf44cc39186bf3d081662358f5211bc59
Mantas <Mantasb77@gmail.com>
2022-12-12 21:25:48 +0000

    Removed DatabaseConnection.kt

--- src/main/kotlin/com/kotlisoft/db/DatabaseConnection.kt
+++
@@ -1,9 +0,0 @@
-package com.kotlisoft.db
-
-import org.ktorm.database.Database
-
-object DatabaseConnection {
-    val database = Database.connect(
-        url = System.getenv("DATABASE_URI").plus("&sslmode=require")
-    )
-}
