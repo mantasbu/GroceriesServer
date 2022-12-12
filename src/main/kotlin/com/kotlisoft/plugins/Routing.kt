d54748fc883fd2ae4673df2561dad3898dca9425
Mantas <Mantasb77@gmail.com>
2022-12-12 23:12:48 +0000

    Initial products scraping logic

--- src/main/kotlin/com/kotlisoft/plugins/Routing.kt
+++ src/main/kotlin/com/kotlisoft/plugins/Routing.kt
@@ -5,6 +5,7 @@ import com.kotlisoft.entities.Notes
 import com.kotlisoft.models.Note
 import com.kotlisoft.models.NoteRequest
 import com.kotlisoft.models.NoteResponse
+import com.kotlisoft.utils.Constants.BASE_PRODUCT_URL
 import io.ktor.http.*
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
@@ -14,12 +15,63 @@ import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.statements.InsertStatement
+import org.jsoup.Jsoup
+import java.lang.StringBuilder
 
 fun Application.configureRouting() {
 
     routing {
         get("/") {
             call.respondText("Groceries Application")
+        }
+    }
+
+    routing {
+        get("/categories") {
+            val doc = Jsoup.connect("https://www.tesco.com/groceries/").get()
+            val result = doc.select(".menu__link--superdepartment")
+                .select("a")
+                .eachAttr("href")
+            call.respondText(result.toString())
+        }
+    }
+
+    routing {
+        get("/products") {
+
+            val doc = Jsoup.connect("https://www.tesco.com/groceries/en-GB/shop/fresh-food/all?page=1&count=48").get()
+            val productLinks = doc.select(".csVOnh")
+                .select("a")
+                .eachAttr("href")
+                .map { it.substringAfter("/groceries/en-GB/products/") }
+
+            val result = StringBuilder()
+
+            productLinks.forEach { productLink ->
+                val productDoc = Jsoup.connect(BASE_PRODUCT_URL.plus(productLink)).get()
+
+                val title = productDoc.select("h1").text().replace("Tesco ", "")
+                val price = productDoc.select(".value")
+                    .select("span")
+                    .firstOrNull {
+                        it.hasAttr("data-auto")
+                    }
+                    ?.text()
+                val discountPrice = productDoc.select(".promo-content-small .offer-text")
+                    .first()
+                    ?.text()
+                    ?.replace(" Clubcard Price", "")
+                    ?.replace("p", "")
+                val offerPeriod = productDoc.select(".dates").first()?.text()
+
+                if (discountPrice != null) {
+                    result.append("$title: price = $price, discounted price = $discountPrice, Validity: $offerPeriod\n")
+                } else {
+                    result.append("$title: price = $price\n")
+                }
+            }
+
+            call.respond(result.toString())
         }
     }
 
