9e5a782d4e11882dffa92651feca4256caba086c
Mantas <Mantasb77@gmail.com>
2022-12-12 21:33:05 +0000

    Cleaned up commented code

--- src/main/kotlin/com/kotlisoft/plugins/Routing.kt
+++ src/main/kotlin/com/kotlisoft/plugins/Routing.kt
@@ -1,10 +1,7 @@
 package com.kotlisoft.plugins
 
-import com.kotlisoft.db.DatabaseConnection
-import com.kotlisoft.db.DatabaseFactory
 import com.kotlisoft.db.DatabaseFactory.dbQuery
 import com.kotlisoft.entities.Notes
-import com.kotlisoft.entities.NotesEntity
 import com.kotlisoft.models.Note
 import com.kotlisoft.models.NoteRequest
 import com.kotlisoft.models.NoteResponse
@@ -15,33 +12,19 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.statements.InsertStatement
-import org.ktorm.dsl.from
-import org.ktorm.dsl.insert
-import org.ktorm.dsl.map
-import org.ktorm.dsl.select
 
 fun Application.configureRouting() {
 
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respondText("Groceries Application")
         }
     }
 
     routing {
         get("/notes") {
-            //val db = DatabaseConnection.database
-//            val notes = db.from(NotesEntity).select()
-//                .map {
-//                    val id = it[NotesEntity.id]
-//                    val note = it[NotesEntity.note]
-//                    Note(id ?: -1, note ?: "")
-//                }
-//            call.respond(notes)
-            DatabaseFactory.init()
             val result = dbQuery {
                 Notes.selectAll().map { rowToNote(it) }
             }
@@ -49,9 +32,6 @@ fun Application.configureRouting() {
         }
 
         post("/notes") {
-
-            DatabaseFactory.init()
-
             val request = call.receive<NoteRequest>()
 
             var statement : InsertStatement<Number>? = null
@@ -69,33 +49,6 @@ fun Application.configureRouting() {
                     data = "Values has been successfully inserted"
                 )
             )
-
-//            val db = DatabaseConnection.database
-//            val request = call.receive<NoteRequest>()
-//            val result = db.insert(NotesEntity) {
-//                set(it.note, request.note)
-//                set(it.id, 4)
-//            }
-//
-//            if (result == 1) {
-//                // Send successfully response to the client
-//                call.respond(
-//                    HttpStatusCode.OK,
-//                    NoteResponse(
-//                        success = true,
-//                        data = "Values has been successfully inserted"
-//                    )
-//                )
-//            } else {
-//                // Send failure response to the client
-//                call.respond(
-//                    HttpStatusCode.BadRequest,
-//                    NoteResponse(
-//                        success = false,
-//                        data = "Failed to insert values."
-//                    )
-//                )
-//            }
         }
     }
 }
