0e1b70d17581752f8a461db817239600d26b846d
Mantas <Mantasb77@gmail.com>
2022-12-19 17:50:17 +0000

    Added rowToNote method

--- src/main/kotlin/com/kotlisoft/models/Note.kt
+++ src/main/kotlin/com/kotlisoft/models/Note.kt
@@ -1,9 +1,21 @@
 package com.kotlisoft.models
 
+import com.kotlisoft.entities.Notes
 import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.ResultRow
 
 @Serializable
 data class Note(
     val id: Int,
     val note: String
 )
+
+fun rowToNote(row: ResultRow?): Note? {
+    if (row == null) {
+        return null
+    }
+    return Note(
+        id = row[Notes.id],
+        note = row[Notes.note]
+    )
+}
