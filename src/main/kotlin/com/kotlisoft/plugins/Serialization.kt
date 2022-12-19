3d18ac82a15a9733c1720dc195a1b0540ed3c6f7
Mantas <Mantasb77@gmail.com>
2022-12-19 18:02:03 +0000

    Added LocalDateSerializer

--- src/main/kotlin/com/kotlisoft/plugins/Serialization.kt
+++ src/main/kotlin/com/kotlisoft/plugins/Serialization.kt
@@ -1,20 +1,21 @@
 package com.kotlisoft.plugins
 
+import com.kotlisoft.db.LocalDateSerializer
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
-import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import java.time.LocalDate
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
-    }
-
-    routing {
-        get("/json/kotlinx-serialization") {
-                call.respond(mapOf("hello" to "world"))
+        json(
+            Json {
+                serializersModule = SerializersModule {
+                    contextual(LocalDate::class, LocalDateSerializer)
+                }
             }
+        )
     }
 }
