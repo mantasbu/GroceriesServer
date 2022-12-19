3c5ef89c0e7e4f6b5b5ca3699cb820ae228aeb42
Mantas <Mantasb77@gmail.com>
2022-12-19 18:04:01 +0000

    Added search route

--- src/main/kotlin/com/kotlisoft/plugins/Routing.kt
+++ src/main/kotlin/com/kotlisoft/plugins/Routing.kt
@@ -2,21 +2,14 @@ package com.kotlisoft.plugins
 
 import com.kotlisoft.db.DatabaseFactory.dbQuery
 import com.kotlisoft.entities.Notes
-import com.kotlisoft.models.Note
-import com.kotlisoft.models.NoteRequest
-import com.kotlisoft.models.NoteResponse
-import com.kotlisoft.utils.Constants.BASE_PRODUCT_URL
+import com.kotlisoft.entities.Products
+import com.kotlisoft.models.rowToNote
+import com.kotlisoft.models.rowToProduct
 import io.ktor.http.*
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
-import org.jetbrains.exposed.sql.statements.InsertStatement
-import org.jsoup.Jsoup
-import java.lang.StringBuilder
 
 fun Application.configureRouting() {
 
@@ -27,51 +20,26 @@ fun Application.configureRouting() {
     }
 
     routing {
-        get("/categories") {
-            val doc = Jsoup.connect("https://www.tesco.com/groceries/").get()
-            val result = doc.select(".menu__link--superdepartment")
-                .select("a")
-                .eachAttr("href")
-            call.respondText(result.toString())
-        }
-    }
-
-    routing {
-        get("/products") {
-
-            val doc = Jsoup.connect("https://www.tesco.com/groceries/en-GB/shop/fresh-food/all?page=1&count=48").get()
-            val productLinks = doc.select(".csVOnh")
-                .select("a")
-                .eachAttr("href")
-                .map { it.substringAfter("/groceries/en-GB/products/") }
-
-            val result = StringBuilder()
-
-            productLinks.forEach { productLink ->
-                val productDoc = Jsoup.connect(BASE_PRODUCT_URL.plus(productLink)).get()
-
-                val title = productDoc.select("h1").text().replace("Tesco ", "")
-                val price = productDoc.select(".value")
-                    .select("span")
-                    .firstOrNull {
-                        it.hasAttr("data-auto")
-                    }
-                    ?.text()
-                val discountPrice = productDoc.select(".promo-content-small .offer-text")
-                    .first()
-                    ?.text()
-                    ?.replace(" Clubcard Price", "")
-                    ?.replace("p", "")
-                val offerPeriod = productDoc.select(".dates").first()?.text()
-
-                if (discountPrice != null) {
-                    result.append("$title: price = $price, discounted price = $discountPrice, Validity: $offerPeriod\n")
-                } else {
-                    result.append("$title: price = $price\n")
+        get("/search") {
+            val name = call.request.queryParameters["name"]
+            if (!name.isNullOrBlank() && name.length > 2) {
+                val result = dbQuery {
+                    Products.selectAll()
+                        .map { rowToProduct(it) }
+                        .filter { it!!.name.lowercase().contains(name.lowercase()) }
                 }
+                call.respond(result)
+            } else if (!name.isNullOrBlank() && name.length < 3) {
+                call.respond(
+                    status = HttpStatusCode.BadRequest,
+                    message = "Name is not valid"
+                )
+            } else {
+                call.respond(
+                    status = HttpStatusCode.BadRequest,
+                    message = "Name cannot be blank"
+                )
             }
-
-            call.respond(result.toString())
         }
     }
 
@@ -82,35 +50,5 @@ fun Application.configureRouting() {
             }
             call.respond(result)
         }
-
-        post("/notes") {
-            val request = call.receive<NoteRequest>()
-
-            var statement : InsertStatement<Number>? = null
-            dbQuery {
-                statement = Notes.insert { note ->
-                    note[id] = 5
-                    note[Notes.note] = request.note
-                }
-            }
-
-            call.respond(
-                HttpStatusCode.OK,
-                NoteResponse(
-                    success = true,
-                    data = "Values has been successfully inserted"
-                )
-            )
-        }
     }
-}
-
-private fun rowToNote(row: ResultRow?): Note? {
-    if (row == null) {
-        return null
-    }
-    return Note(
-        id = row[Notes.id],
-        note = row[Notes.note]
-    )
 }
